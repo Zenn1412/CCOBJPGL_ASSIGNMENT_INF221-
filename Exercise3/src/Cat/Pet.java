54edb9aded275e58eaded2328ce873a786a77a0a
Paulo Agapito <111472748+Zenn1412@users.noreply.github.com>
2023-05-15 09:13:28 +0800

    Pet.java

---
+++ Exercise3/src/Cat/Pet.java
@@ -0,0 +1,9 @@
+public class Pet implements Sphinxie {
+
+    @Override
+    public void Cat() {
+
+        System.out.println("UwU");
+        System.out.println("My Sphinxie is Sleeping");
+    };
+}
